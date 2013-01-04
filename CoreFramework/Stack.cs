3953ae77982a1900e42532a75188181025628810
AlexShirshov_cp <SND\AlexShirshov_cp>
2013-01-04 11:08:06 +0000

    PreserveStackTrace method added

---
+++ CoreFramework/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace CoreFramework.Debugging
+{
+    public static class Stack
+    {
+        /// <summary>
+        /// Stolen from <a href="http://weblogs.asp.net/fmarguerie/archive/2008/01/02/rethrowing-exceptions-and-preserving-the-full-call-stack-trace.aspx">here</a>
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void PreserveStackTrace(Exception exception)
+        {
+            MethodInfo preserveStackTrace = typeof(Exception).GetMethod("InternalPreserveStackTrace",
+              BindingFlags.Instance | BindingFlags.NonPublic);
+            preserveStackTrace.Invoke(exception, null);
+        }
+
+    }
+}
--- CoreFramework/Timer.cs
+++ CoreFramework/Timer.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CoreFramework.Debuging
+namespace CoreFramework.Debugging
 {
     public class OutputTimer : IDisposable
     {
