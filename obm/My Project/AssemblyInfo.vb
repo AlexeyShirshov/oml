59ef04447b36c6317a8487c6279363ce60c4d24d
AlexeyShirshov <alex.shirshov@gmail.com>
2018-01-20 15:18:39 +0300

    update coreframework

--- obm/My Project/AssemblyInfo.vb
+++ obm/My Project/AssemblyInfo.vb
@@ -34,7 +34,7 @@ Imports System.Runtime
 ' by using the '*' as shown below:
 
 <Assembly: AssemblyVersion("4.1.7.1")>
-<Assembly: AssemblyFileVersion("4.1.134.18006")>
+<Assembly: AssemblyFileVersion("4.1.136.18016")>
 
 '<Assembly: FileIOPermission(SecurityAction.RequestRefuse)> 
 <Assembly: ConstrainedExecution.ReliabilityContract(ConstrainedExecution.Consistency.MayCorruptInstance, ConstrainedExecution.Cer.MayFail)> 
