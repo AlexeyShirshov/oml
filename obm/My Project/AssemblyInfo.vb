a318997ac4baf0dc13f0aaf1218867cc204d415a
AlexeyShirshov <alex.shirshov@gmail.com>
2018-05-19 18:18:07 +0300

    type map optimization

--- obm/My Project/AssemblyInfo.vb
+++ obm/My Project/AssemblyInfo.vb
@@ -34,7 +34,7 @@ Imports System.Runtime
 ' by using the '*' as shown below:
 
 <Assembly: AssemblyVersion("4.1.7.1")>
-<Assembly: AssemblyFileVersion("4.1.174.18138")>
+<Assembly: AssemblyFileVersion("4.1.176.18138")>
 
 '<Assembly: FileIOPermission(SecurityAction.RequestRefuse)> 
 <Assembly: ConstrainedExecution.ReliabilityContract(ConstrainedExecution.Consistency.MayCorruptInstance, ConstrainedExecution.Cer.MayFail)> 
