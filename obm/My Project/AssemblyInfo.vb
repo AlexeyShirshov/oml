2680009b44033db913dbae8a3f268274f2a71d74
AlexeyShirshov <alex.shirshov@gmail.com>
2018-06-28 15:41:08 +0300

    create nuget pack

--- obm/My Project/AssemblyInfo.vb
+++ obm/My Project/AssemblyInfo.vb
@@ -34,7 +34,7 @@ Imports System.Runtime
 ' by using the '*' as shown below:
 
 <Assembly: AssemblyVersion("4.1.7.1")>
-<Assembly: AssemblyFileVersion("4.1.185.18178")>
+<Assembly: AssemblyFileVersion("4.1.186.18179")>
 
 '<Assembly: FileIOPermission(SecurityAction.RequestRefuse)> 
 <Assembly: ConstrainedExecution.ReliabilityContract(ConstrainedExecution.Consistency.MayCorruptInstance, ConstrainedExecution.Cer.MayFail)> 
