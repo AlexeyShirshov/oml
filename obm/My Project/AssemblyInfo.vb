c24f6dd52f96f6257745367433cb7917f84b3b0f
AlexeyShirshov <alex.shirshov@gmail.com>
2019-04-26 23:00:17 +0300

    build release

--- obm/My Project/AssemblyInfo.vb
+++ obm/My Project/AssemblyInfo.vb
@@ -34,7 +34,7 @@ Imports System.Runtime
 ' by using the '*' as shown below:
 
 <Assembly: AssemblyVersion("4.1.7.1")>
-<Assembly: AssemblyFileVersion("4.1.208.19110")>
+<Assembly: AssemblyFileVersion("4.1.209.19116")>
 
 '<Assembly: FileIOPermission(SecurityAction.RequestRefuse)> 
 <Assembly: ConstrainedExecution.ReliabilityContract(ConstrainedExecution.Consistency.MayCorruptInstance, ConstrainedExecution.Cer.MayFail)> 
